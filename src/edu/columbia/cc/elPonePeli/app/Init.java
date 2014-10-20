4e381e1ef1c07d21c1e3b99d9482dce09a27c39b
Fei Liu <aaron11111.china@gmail.com>
2014-10-19 21:01:53 -0400

    update

--- src/edu/columbia/cc/elPonePeli/app/Init.java
+++ src/edu/columbia/cc/elPonePeli/app/Init.java
@@ -3,54 +3,46 @@ package edu.columbia.cc.elPonePeli.app;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 
-public class Init
-{
-	AWSCredentials awsCredentials;
+public class Init {
+    AWSCredentials awsCredentials;
 
-	public static void main(String[] args)
-	{
-		Init init = new Init();
-		
-		init.doCredentials();
-		init.createBucket();
-		init.createDistribution();
-		init.createDatabase();
-		init.createLiveStreaming();
-	}
-	
-	public void doCredentials()
-	{
-		this.awsCredentials = new BasicAWSCredentials(AwsCredentialConstants.ACCESS.getValue(), AwsCredentialConstants.SECRET.getValue());
-	}
-	
-	public void createBucket()
-	{
-		VideoStore store = new VideoStore()
-							.withCredentials(awsCredentials);
-		store.initialize();
-	}
-	
-	public void createDistribution()
-	{
-		OnDemandDistributor distributor = new OnDemandDistributor()
-							.withAWSCredentials(awsCredentials)
-							.withBucketName(VideoStore.bucketName);
-		distributor.createWebDistribution();
-		distributor.createRtmpDistribution();
-	}
-	
-	public void createDatabase()
-	{
-		DatabaseHelper helper = new DatabaseHelper()
-							.withCredentials(awsCredentials);
-		helper.initialize();
-		
-	}
-	
-	public void createLiveStreaming() {
-		LiveStreamingHelper lsh = new LiveStreamingHelper(awsCredentials);		
-	}
-	
-	
+    public static void main(String[] args) {
+        Init init = new Init();
+        init.doCredentials();
+        init.createBucket();
+        init.createDistribution();
+        init.createDatabase();
+        init.createLiveStreaming();
+    }
+
+    public void doCredentials() {
+        this.awsCredentials = new BasicAWSCredentials(
+                AwsCredentialConstants.ACCESS.getValue(),
+                AwsCredentialConstants.SECRET.getValue());
+    }
+
+    public void createBucket() {
+        VideoStore store = new VideoStore().withCredentials(awsCredentials);
+        store.initialize();
+    }
+
+    public void createDistribution() {
+        OnDemandDistributor distributor = new OnDemandDistributor()
+                .withAWSCredentials(awsCredentials).withBucketName(
+                        VideoStore.bucketName);
+        distributor.createWebDistribution();
+        distributor.createRtmpDistribution();
+    }
+
+    public void createDatabase() {
+        DatabaseHelper helper = new DatabaseHelper()
+                .withCredentials(awsCredentials);
+        helper.initialize();
+
+    }
+
+    public void createLiveStreaming() {
+        LiveStreamingHelper lsh = new LiveStreamingHelper(awsCredentials);
+    }
 
 }
--- src/edu/columbia/cc/elPonePeli/app/LiveStreamingHelper.java
+++ src/edu/columbia/cc/elPonePeli/app/LiveStreamingHelper.java
@@ -15,59 +15,63 @@ import com.amazonaws.services.cloudformation.model.Parameter;
 
 public class LiveStreamingHelper {
 
-	private AmazonCloudFormation stackbuilder = null;
-	private final String streamingTemplateURL = "https://s3.amazonaws.com/cfwowza/live-http-streaming-wowza-media-server-3-6-using-cloudfront.txt";
-	private final String streamingStackName = "elPonePeliStreaming";
-	
-	public LiveStreamingHelper(AWSCredentials credentials) {
-		stackbuilder = new AmazonCloudFormationClient(credentials);
-		Region region = Region.getRegion(Regions.US_EAST_1);
-		stackbuilder.setRegion(region);
+    private AmazonCloudFormation stackbuilder = null;
+    private final String streamingTemplateURL = "https://s3.amazonaws.com/cfwowza/live-http-streaming-wowza-media-server-3-6-using-cloudfront.txt";
+    private final String streamingStackName = "elPonePeliStreamingtest";
+
+    public LiveStreamingHelper(AWSCredentials credentials) {
+        stackbuilder = new AmazonCloudFormationClient(credentials);
+        Region region = Region.getRegion(Regions.US_EAST_1);
+        stackbuilder.setRegion(region);
 
         System.out.println("===========================================");
         System.out.println("Starting Live Streaming Formation");
         System.out.println("===========================================\n");
-        
+
         String logicalResourceName = "SampleNotificationTopic";
         createFormation(streamingStackName, logicalResourceName);
-	}
-	
-	public void createFormation(String stackName, String logicalResourceName)  {
-    	
-		try {
-			
+    }
+
+    public void createFormation(String stackName, String logicalResourceName) {
+
+        try {
+
             // Create a stack
-			Collection<Parameter> params = new ArrayList<Parameter>();
-			Parameter pInstanceType = new Parameter().withParameterKey("InstanceType").withParameterValue("t2.micro");
-			params.add(pInstanceType);
-			Parameter pKeyPair = new Parameter().withParameterKey("KeyPair").withParameterValue("fbtest");
-			params.add(pKeyPair);
-			Parameter pLicKey = new Parameter().withParameterKey("WowzaLicenseKey").withParameterValue("SVRT3-HEKkZ-zRp6C-xPTA7-b4a8P-VwR9R-7U6W7f6TXXxt");
-			params.add(pLicKey);
-			
-			
+            Collection<Parameter> params = new ArrayList<Parameter>();
+            Parameter pInstanceType = new Parameter().withParameterKey(
+                    "InstanceType").withParameterValue("t2.micro");
+            params.add(pInstanceType);
+            Parameter pKeyPair = new Parameter().withParameterKey("KeyPair")
+                    .withParameterValue("fbtest");
+            params.add(pKeyPair);
+            Parameter pLicKey = new Parameter().withParameterKey(
+                    "WowzaLicenseKey").withParameterValue(
+                    "SVRT3-HEKkZ-zRp6C-xPTA7-b4a8P-VwR9R-7U6W7f6TXXxt");
+            params.add(pLicKey);
+
             CreateStackRequest createRequest = new CreateStackRequest()
-            									.withTemplateURL(streamingTemplateURL)
-            									.withStackName(streamingStackName)
-            									.withParameters(params);
-            System.out.println("Creating a stack called " + createRequest.getStackName() + ".");
+                    .withTemplateURL(streamingTemplateURL)
+                    .withStackName(streamingStackName).withParameters(params);
+            System.out.println("Creating a stack called "
+                    + createRequest.getStackName() + ".");
             stackbuilder.createStack(createRequest);
 
-     
         } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which means your request made it "
-                    + "to AWS CloudFormation, but was rejected with an error response for some reason.");
+            System.out
+                    .println("Caught an AmazonServiceException, which means your request made it "
+                            + "to AWS CloudFormation, but was rejected with an error response for some reason.");
             System.out.println("Error Message:    " + ase.getMessage());
             System.out.println("HTTP Status Code: " + ase.getStatusCode());
             System.out.println("AWS Error Code:   " + ase.getErrorCode());
             System.out.println("Error Type:       " + ase.getErrorType());
             System.out.println("Request ID:       " + ase.getRequestId());
         } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with AWS CloudFormation, "
-                    + "such as not being able to access the network.");
+            System.out
+                    .println("Caught an AmazonClientException, which means the client encountered "
+                            + "a serious internal problem while trying to communicate with AWS CloudFormation, "
+                            + "such as not being able to access the network.");
             System.out.println("Error Message: " + ace.getMessage());
         }
-	}
+    }
 
 }
